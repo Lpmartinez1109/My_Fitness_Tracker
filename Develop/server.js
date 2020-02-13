d8c5881dddbb8de11544b260ff8f56c61cc15e78
LPMartinez <lpmartinez1109@gmail.com>
2020-02-12 22:15:07 -0600

    heruko connection made

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -7,12 +8,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(logger("dev"));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://fitness_tracker:marvel4life@ds147520.mlab.com:47520/heroku_kg2q12mk", {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
 app.use(require("./routes/apiRoutes.js"));
